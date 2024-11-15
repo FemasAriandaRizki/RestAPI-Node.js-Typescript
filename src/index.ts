9bae857a0600d396b2876173c21ae03549282d8e
femas arianda <femasariandarizki@students.undip.ac.id>
2024-11-16 01:39:16 +0700

    setup node.js with express.js n ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("hello world!");
+import express, { Application, Request, Response, NextFunction } from "express";
+
+const app = express();
+const port: Number = 4000;
+
+app.use("/health", (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send({ status: "200" });
+});
+
+app.listen(port, () => console.log(`Server is listening on port ${port}`));
